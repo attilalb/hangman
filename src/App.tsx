8b02cca49385004f3abe982343a0ba7653b186df
attilalb <115696445+attilalb@users.noreply.github.com>
2023-05-16 00:28:48 +0200

    added effect hook to reload game

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import HangmanWord from './HangmanWord';
 import Keyboard from './Keyboard';
 import words from './wordList.json';
 
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)];
-  });
+  const [wordToGuess, setWordToGuess] = useState(getWord);
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   const mistakes = guessedLetters.filter(
@@ -45,11 +47,50 @@ function App() {
       document.removeEventListener('keypress', handler);
     };
   }, [guessedLetters]);
+  React.useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+
+      if (key!=="Enter") return;
+
+      e.preventDefault();
+      setGuessedLetters([]);
+      setWordToGuess(getWord())
+    };
+
+    document.addEventListener('keypress', handler);
+
+    return () => {
+      document.removeEventListener('keypress', handler);
+    };
+  }, [guessedLetters]);
   return (
     <div className="container is-flex is-flex-direction-column is-align-items-center">
-      <div className="text is-size-3 has-text-centered">
-        {isWinner && 'You won!'}
-        {isLoser && 'You lost!'}
+      <div
+        className={
+          'modal is-size-3 has-text-centered ' + (isWinner || isLoser)
+            ? 'is-active'
+            : ''
+        }>
+        <div className="modal-content">
+          <div className="box">
+            {isWinner && (
+              <div>
+                <h2 className="title has-text-primary has-text-centered">
+                  Congratulations! You won!
+                </h2>
+                <button className="button is-primary">Play again</button>
+              </div>
+            )}
+            {isLoser && (
+              <div>
+                <h2 className="title has-text-danger has-text-centered">
+                  You didn't guess the word this time!
+                </h2>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
       <HangmanDrawing guessCount={mistakes.length} />
       <HangmanWord
